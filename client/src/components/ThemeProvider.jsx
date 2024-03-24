8e3b3b573b4ed3677261e57ffd0e247b9292389a
Hamzshaa <trident32000@gmail.com>
2024-03-24 23:31:38 +0300

    problem on dark mode toggler fixed

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 export default function ThemeProvider({ children }) {
   const { theme } = useSelector((state) => state.theme);
   return (
-    <div className={theme}>
+    <div className={theme == "dark" ? "dark" : ""}>
       <div className="bg-slate-200 dark:bg-slate-900 text-slate-900 dark:text-slate-100">
         {children}
       </div>
