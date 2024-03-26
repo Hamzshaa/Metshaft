001b9bae6fa86764ec38c11bb0392e5f8f41892b
Hamzshaa <trident32000@gmail.com>
2024-03-26 20:03:54 +0300

    logo changed in the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,24 @@ import { Button } from "flowbite-react";
 import Banner from "../components/BannerComponent";
 import Logo from "../components/Logo";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import lightLogo from "../assets/logo_light.png";
+import darkLogo from "../assets/logo_dark.png";
 
 export default function Home() {
+  const { theme } = useSelector((state) => state.theme);
   return (
     <div className="relative">
       <div className="h-[var(--body-height)] w-full bg-slate-50 dark:bg-slate-800 absolute top-0 bg-opacity-90 dark:bg-opacity-80 -z-0"></div>
       <div className="h-[var(--body-height)] bg-logo-bgg bg-[url('./assets/logo_bg.jpg')] bg-no-repeat bg-cover bg-center"></div>
       <div className="z-20 absolute top-8 w-full h-[var(--body-height)] mt-20 sm:mt-16 md:mt-16">
-        <Logo />
+        {/* <Logo /> */}
+        <div className="h-[200px] items-center flex justify-center">
+          <img
+            src={theme == "dark" ? darkLogo : lightLogo}
+            className="overflow-hidden -mb-10 "
+          />
+        </div>
         <div className="mt-10 text-center max-w-[600px] mx-auto">
           <div className="h-[200px] relative">
             <div
@@ -31,7 +41,7 @@ export default function Home() {
             </div>
             <Link to="/add">
               <Button
-                size="xs"
+                size="md"
                 color="yellow"
                 className="absolute bottom-0 left-0 w-1/2 mx-[25%]"
               >
