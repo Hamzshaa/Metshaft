6d30fc57d7c023282dee66940e67a760ac48fad1
Hamzshaa <trident32000@gmail.com>
2024-03-31 00:19:17 +0300

    layout for single book page changed

--- client/src/pages/Book.jsx
+++ client/src/pages/Book.jsx
@@ -54,7 +54,7 @@ export default function Book() {
         <div className="max-w-[480px] sm:max-w-[700px] md:max-w-[900px] mx-auto md:flex md:gap-4 md:items-center">
           {book.img ? (
             <div className="w-full max-w-[360px] sm:max-w-[400px] mx-auto md:w-[300px] order-2">
-              <img src={book?.img} alt={book.title} className="cover" />
+              <img src={book?.img} alt={book.title} className="cover w-full" />
             </div>
           ) : (
             <div className="order-2 mx-auto text-slate-600 dark:text-slate-500 flex flex-col gap-4 items-center">
