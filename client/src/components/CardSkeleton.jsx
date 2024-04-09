5cadb20e6753f1cdd04ab1b95d637d40c7322f57
Hamzshaa <trident32000@gmail.com>
2024-04-10 01:24:22 +0300

    skeleton refresh added to recent users table

---
+++ client/src/components/CardSkeleton.jsx
@@ -0,0 +1,15 @@
+// import Skeleton from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
+
+export default function CardSkeleton() {
+  return (
+    <div className="">
+      {/* <div className="">
+        <Skeleton circle width={40} height={40} />
+      </div>
+      <div className="">
+        <Skeleton count={4} />
+      </div> */}
+    </div>
+  );
+}
--- client/src/components/DashboardRecentUsersTable.jsx
+++ client/src/components/DashboardRecentUsersTable.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 
 export default function DashboardRecentUsersTable() {
   const [recentUsers, setRecentUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
   const options = { year: "numeric", month: "long", day: "numeric" };
 
   useEffect(() => {
     const fetchRecentUsers = async () => {
+      setLoading(true);
       try {
         const res = await fetch("/api/user?limit=5");
         const data = await res.json();
@@ -15,8 +17,10 @@ export default function DashboardRecentUsersTable() {
         if (res.ok) {
           setRecentUsers(data.users);
         }
+        setLoading(false);
       } catch (error) {
         console.log(error.message);
+        setLoading(false);
       }
     };
     fetchRecentUsers();
@@ -33,6 +37,29 @@ export default function DashboardRecentUsersTable() {
           SEE ALL <MdArrowOutward />
         </Link>
       </div>
+
+      {loading && (
+        <div className="flex flex-col gap-6 mt-8">
+          {Array(5)
+            .fill(0)
+            .map((item, index) => (
+              <div className="flex flex-col gap-2 mx-3 mb-0" key={index}>
+                <div className="flex justify-between items-end">
+                  <div className="flex gap-3 items-center ">
+                    <div className="w-16 h-16 md:w-10 md:h-10 rounded-full object-cover max-w-16 bg-gray-300 dark:bg-gray-400 animate-pulse"></div>
+                    <div className="flex flex-col">
+                      <div className="w-32 h-5 bg-gray-300 dark:bg-gray-400 animate-pulse rounded-sm"></div>
+                      <div className="w-40 h-3 bg-gray-300 dark:bg-gray-500 animate-pulse mt-3 rounded-sm"></div>
+                    </div>
+                  </div>
+                  <div className="pb-2">
+                    <div className="w-20 h-2 bg-gray-300 dark:bg-gray-500 animate-pulse rounded-sm"></div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      )}
       <div className="flex flex-col gap-6 mx-3 mb-4 mt-8">
         {recentUsers.length != 0 &&
           recentUsers.map((user, index) => (
